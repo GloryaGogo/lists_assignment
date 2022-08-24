55416aa4c60c306c8c47c30210c81e3ec0dc2e8e
gloryagogo <gloria.nakiduuli@gmail.com>
2022-08-24 20:37:26 +0100

    assignment

---
+++ asign_lists.py
@@ -0,0 +1,42 @@
+from csv import list_dialects
+
+
+1
+# user_input= int(input("enter number:"))
+# u = 1
+# if u<=1:
+#     print(user_input,"+" , u ,"=" ,user_input+u )
+# u=u+1 
+2
+# list =['abc', 'xyz', 'aba', '1221']
+# def sample_list(list):
+#     ctr = 0
+#     for list in list:
+#         if len(list) > 1 and list[0] == [-1]:
+#             ctr = 1
+#             return ctr
+# print(sample_list(['abc', 'xyz','aba', '1221']))
+
+
+
+3
+# fruits = ["Apple", "Banana", "Melon", "Banana", "Cherry", "Banana"]
+# fruit = set(fruits)
+# print(list(fruit))
+
+4
+# fruits = ["Apple", "Banana", "Melon", "Banana", "Cherry", "Banana"]
+# my_final = []
+# [my_final.append(n) for n in fruits if n not in my_final]
+# print(my_final)
+
+5
+list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print(list[0:4:5])
+
+
+6
+# sqr_list = []
+# for i in range(1,30):
+#     sqr_list.append(i*i)
+#     print(*sqr_list)
